9b641549b65a5e06561214cbd5102d79d0efaf35
Eduardo Coello <jeck24@hotmail.com>
2020-10-14 18:52:48 -0500

    kind of makes sense

--- script.js
+++ script.js
@@ -13,27 +13,56 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+//The homework starts here
 function generatePassword(){
+
+  function randomCapitalLetter(){
+    return String.fromCharCode((Math.random()*26)+65);
+  }
+  
+  //This part is the randomizer for the lower letter
+  function randomLowerLetter(){
+    return String.fromCharCode((Math.random()*26)+97);
+  }
+  
+  //This part is the randomizer for the number
+  function randomNumericCharacters(){
+    return String.fromCharCode((Math.random()*10)+48)
+  }
+  
+  //This part is the randomizer for the special character
+  function randomSpecialCharacters(){
+    var specialCharacter = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    return specialCharacter[Math.floor(Math.random()*specialCharacter.length)];
+  }
+
+  //This part is gathering the password's characteristics from the user
+  var finalPassword = "";
   var passwordLength = prompt ("How many characters would you like in your password?");
   var capitalLetter = confirm ("Would you like your password to include capital letters?");
   var lowerLetter = confirm ("Would you like your password to include lower case letters?");
   var numericCharacters = confirm ("Would you like your password to include numbers?");
   var specialCharacters = confirm ("Would you like your password to include special characters?");
 
-  const safePassword = {
-    capital : randomCapitalLetter(),
-    lower : randomLowerLetter(),
-    numeric : randomNumericCharacters(),
-    special : randomSpecialCharacters()
+  //This part is the randomizer for the capital letter
+
+  
+  //This part is defining the characteristics of the password
+  var safePassword = {
+    capitalLetter : randomCapitalLetter(),
+    lowerLetter : randomLowerLetter(),
+    numericCharacters : randomNumericCharacters(),
+    specialCharacters : randomSpecialCharacters()
   };
 
-  const characteristic = [{capitalLetter},{lowerLetter},{numericCharacters},{specialCharacters}].filter(item => Object.values(item)[0]);
+  //not understanding
+  var characteristic = [{capitalLetter},{lowerLetter},{numericCharacters},{specialCharacters}].filter(item => Object.values(item)[0]);
 
-  var finalPassword = "";
 
   for (var i=0; i<passwordLength;i++){
     characteristic.forEach(type =>{
-      const funcName = Object.keys(type)[0];
+      var funcName = Object.keys(type)[0];
       finalPassword += safePassword[funcName]();
     });
   }
@@ -43,19 +72,4 @@ function generatePassword(){
   return password;
 }
 
-function randomCapitalLetter(){
-  return String.fromCharCode((Math.random()*26)+65);
-}
 
-function randomLowerLetter(){
-  return String.fromCharCode((Math.random()*26)+97);
-}
-
-function randomNumericCharacters(){
-  return String.fromCharCode((Math.random()*10)+48)
-}
-
-function randomSpecialCharacters(){
-  var specialCharacter = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  return specialCharacter[Math.floor(Math.random()*specialCharacter.length)];
-}
